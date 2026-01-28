e651fdfedeae3968a08ecf767957680cf8a4d793
Eramsbilly <eramsbillionz8@gmail.com>
2026-01-28 16:59:16 -0500

    Update user.py

--- routes/user.py
+++ routes/user.py
@@ -1,1 +1,14 @@
+from flask import Blueprint
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from models import User
 
+user_bp = Blueprint("user", __name__)
+
+@user_bp.route("/me")
+@jwt_required()
+def me():
+    user = User.query.get(get_jwt_identity())
+    return {
+        "email": user.email,
+        "balance": user.balance
+    }
